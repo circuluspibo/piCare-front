a8374965a1108733ad30402114d411e9aa6fd532
KiYoon <yes505304@circul.us>
2025-12-17 15:10:37 +0900

    update: 그리기 음성 개선

---
+++ src/assets/data/DrawTools.js
@@ -0,0 +1,16 @@
+const DRAW_TOOLS = [
+  {
+    name: "Pencil",
+    style: "bg-green-400",
+  },
+  {
+    name: "Eraser",
+    style: "border-red-600 text-red-600",
+  },
+  {
+    name: "X",
+    style: "bg-red-700 text-white",
+  },
+];
+
+export default DRAW_TOOLS;
