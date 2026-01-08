4c21b22f0000e65c0ccb7179e1628a70da8534db
rippertnt <rippertnt@circul.us>
2026-01-08 17:54:12 +0900

    fix: url 수정

--- src/api/loggerService.js
+++ src/api/loggerService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const logClient = axios.create({
-  baseURL: "", // MongoDB server
+  baseURL: "http://localhost:5000", // MongoDB server
 });
 
 export const postLogger = async (logData) => {
   try {
-    await logClient.post("/api/logs", logData);
+    await logClient.post("/v1/logs", logData);
   } catch (error) {
     console.error("[Faild to save log data] : ", error);
   }
